e6d82dece42d8bce3cdf3d7f787e8c0528f0bbda
Andreas Fischer <_@ndreas.de>
2022-09-18 13:09:35 +0200

    Primitive HTML content, if there is no template

--- pelican/plugins/pelican_bibtex_reader/pelican_bibtex_reader.py
+++ pelican/plugins/pelican_bibtex_reader/pelican_bibtex_reader.py
@@ -6,32 +6,29 @@ from pelican.readers import BaseReader
 
 from . import bibtex
 
-# Create a new reader class, inheriting from the pelican.reader.BaseReader
 class BibTeXReader(BaseReader):
-    enabled = True  # Yeah, you probably want that :-)
+    enabled = True
 
-    # The list of file extensions you want this reader to match with.
-    # If multiple readers were to use the same extension, the latest will
-    # win (so the one you're defining here, most probably).
     file_extensions = ['bib']
 
     # You need to have a read method, which takes a filename and returns
     # some content and the associated metadata.
     def read(self, filename):
         metadata = {'title': os.path.basename(filename)[:-4],
-                    'template': 'publications',
                     'date': str(dt.date.today())}
 
         with open(filename, 'rb') as raw_file:
             bibstring = bibtex.get_decoded_string_from_file(raw_file)
-            metadata['elements'] = bibtex.get_bibitems(bibstring).entries
+            metadata['bibtexdatabase'] = bibtex.get_bibitems(bibstring).entries
 
         metadata.update(get_keyvalue_pairs(filename))
 
         parsed = {}
         for key, value in metadata.items():
             parsed[key] = self.process_metadata(key, value)
-        return "Some content", parsed
+
+        content = generate_HTML_content(metadata['bibtexdatabase'])
+        return content, parsed
 
 def get_keyvalue_pairs(filename):
     result = {}
@@ -43,9 +40,18 @@ def get_keyvalue_pairs(filename):
             result[key] = value
     return result
 
+def generate_HTML_content(bibelements):
+    result = "".join(f"<li>{key_value_to_HTML(e)}</li>"
+            for e in bibelements)
+    return f"<ol>{result}</ol>"
+
+def key_value_to_HTML(dictionary):
+    result = "".join(f"<li><strong>{k}: </strong>{v}</li>"
+                     for k, v in dictionary.items())
+    return f"<ul style='list-style-type: none;'>{result}</ul>"
+
 def add_reader(readers):
     readers.reader_classes['bib'] = BibTeXReader
 
-# This is how pelican works.
 def register():
     signals.readers_init.connect(add_reader)
